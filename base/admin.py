ab27373044d30f849d56dc887e14f4dcafafd7ce
Rquaicoo <russellquaicoo1@gmail.com>
2023-08-23 00:02:12 +0000

    fixed daat integrity bug

--- base/admin.py
+++ base/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Student, Lecturer, Course
+from .models import Student, Lecturer, Course, User
 # Register your models here.
 
-
+admin.site.register(User)
 admin.site.register(Student)
 admin.site.register(Lecturer)
 admin.site.register(Course)
